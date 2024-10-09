bf764c5c7ba07e395edc8d64264ce095cfb2c527
Thiago Roberto <thiagoroberto20@gmail.com>
2024-10-08 21:59:44 -0300

    teste

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+     name="img-process-trxc",
+     version="0.0.1",   
+     author="Thiago Roberto",
+     description="Image Processing Package using Skimage",
+     long_description=page_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/thiagoroberto20/image-processing-package",
+     packages=find_packages(),
+     install_requires=requirements,
+     python_requires='>=3.5',
+)
